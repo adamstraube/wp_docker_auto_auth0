63b42e239333c419d1d4ed0ad1310cfacddaedce
Adam Straube <adam.straube@auth0.com>
2022-09-08 12:56:32 +1000

    tweak

--- docker/wordpress-cli/install.sh
+++ docker/wordpress-cli/install.sh
@@ -7,4 +7,5 @@ wp core install \
     --admin_email="${WORDPRESS_ADMIN_EMAIL}"
 
 wp option update siteurl "${WORDPRESS_WEBSITE_URL}";
+wp option update users_can_register 1
 wp plugin install auth0 --activate
